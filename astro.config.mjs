06fdcfb7a2d6cfc275984a2e5c59fac51a6e1dd0
profWilliamArte <arteagawilliam13@hotmail.com>
2024-02-07 09:40:55 -0400

    2er commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://profWilliamArte.github.io',
+	base: 'astroblog',
 	integrations: [mdx(), sitemap()],
+
 });
